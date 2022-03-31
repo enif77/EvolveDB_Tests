e9778ccd2319a890e81013c70761f21612e68fb7
Přemysl Fára <pfar@ezpada.com>
2022-03-31 08:29:09 +0200

    Cleaned up the create-db script, update-db script, placeholders.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This repo shows some ideas, how to use the evolve-db tool for managing changes i
 ## Links
 
 * https://evolve-db.netlify.app/
+* https://evolve-db.netlify.app/concepts/
 * https://evolve-db.netlify.app/getting-started/tool/
 
 ## Howtos
@@ -12,7 +13,7 @@ This repo shows some ideas, how to use the evolve-db tool for managing changes i
 The `create-db` script is set up for MSSQL 2019 running on linux. I am using Ubuntu Server 20.04.
 
 To create a new DB, use the `args-master.txt`. It cereates the `TestDB2` database. It modifies the `master` database,
-so it creates migration table in it. Changes in the database server level can be then tracked too.
+so it creates migration table in it, showing that changes on the database server level can be then tracked too.
 
 If you do not want to use database changes tracking in the `master` database, execute script(s) from the `sql-master`
 directory manually.
@@ -20,13 +21,13 @@ directory manually.
 Start with creating a new DB:
 
 ```
-evolve.exe info @args-master.txt
-evolve.exe migrate @args-master.txt
+./evolve.exe info @args-master.txt
+./evolve.exe migrate @args-master.txt
 ```
 
 The TestDB2 database migrations are done by:
 
 ```
-evolve.exe info @args-TestDB2.txt
-evolve.exe migrate @args-TestDB2.txt
+./evolve.exe info @args-TestDB2.txt
+./evolve.exe migrate @args-TestDB2.txt
 ```
---
+++ sql-master/V2__Update_DB_settings.sql
@@ -0,0 +1,108 @@
+-- evolve-tx-off
+
+-- This is just an example of "changing" a DB setup after it is created.
+
+USE [master]
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+    EXEC [${database-name}].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [${database-name}] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [${database-name}] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [${database-name}] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [${database-name}] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [${database-name}] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [${database-name}] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [${database-name}] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [${database-name}] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [${database-name}] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [${database-name}] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+
+ALTER DATABASE [${database-name}] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [${database-name}] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+
+ALTER DATABASE [${database-name}] SET QUERY_STORE = OFF
+GO
+
+ALTER DATABASE [${database-name}] SET  READ_WRITE 
+GO
